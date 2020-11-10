ed2d263cb571401e767e8b33ff3f24ac12d47fa5
gam1 <gam1@3ds.com>
2020-11-10 17:24:46 -0500

    Rate Table presentation

--- rates-mockup/src/features/RateRow/RateRow.js
+++ rates-mockup/src/features/RateRow/RateRow.js
@@ -1,5 +1,20 @@
+import React from "react";
 import "./style.css";
 
-export function RateRow() {
-  return <div className="RateRow-container">Rows</div>;
+export function RateRow(props) {
+  const { row } = props;
+
+  const percentValue = (value) => `${Number(value).toFixed(3)}%`;
+  const roundedValue = (value) => `$${Number(value).toFixed()}`;
+
+  return (
+    <tr style={{ textAlign: "center" }}>
+      <td>{row.lenderName}</td>
+      <td>{row.loanType}</td>
+      <td>{percentValue(row.interestRate)}</td>
+      <td>{roundedValue(row.closingCosts)}</td>
+      <td>{roundedValue(row.monthlyPayment)}</td>
+      <td>{percentValue(row.apr)}</td>
+    </tr>
+  );
 }
--- rates-mockup/src/features/RateTable/RateTable.js
+++ rates-mockup/src/features/RateTable/RateTable.js
@@ -2,6 +2,7 @@ import { Spinner, HTMLTable } from "@blueprintjs/core";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import { useSelector, shallowEqual } from "react-redux";
+import { RateRow } from "../RateRow";
 import "./style.css";
 
 export function RateTable() {
@@ -16,13 +17,16 @@ export function RateTable() {
   return (
     <>
       {ratesLoading && <Spinner />}
+      {ratesError && (
+        <div className="RateTable-error"> Oops. Something went wrong. </div>
+      )}
       {!ratesData ? (
         <div className="RateTable-nodata">
           Fill in the required fields to see your great rates!!!
         </div>
       ) : ratesData.length > 0 ? (
         <div className="RateTable-container">
-          <HTMLTable bordered={true} interactive={true} striped={true}>
+          <HTMLTable striped={true}>
             <thead>
               <tr>
                 <th>Lender</th>
@@ -33,7 +37,11 @@ export function RateTable() {
                 <th>APR</th>
               </tr>
             </thead>
-            <tbody></tbody>
+            <tbody>
+              {ratesData.map((rate) => (
+                <RateRow row={rate} />
+              ))}
+            </tbody>
           </HTMLTable>
         </div>
       ) : (
